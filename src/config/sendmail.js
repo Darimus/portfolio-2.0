258116cf865b470e6d675f31aa35094071ea6a5d
Darimus <bruno.passos@outlook.com.br>
2022-10-20 10:32:08 -0300

    teste

---
+++ src/config/sendmail.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer');
+const SMTP_CONFIG = require('./smtp.js')
+
+const transporter = nodemailer.createTransport({
+  host: SMTP_CONFIG.host,
+  port: SMTP_CONFIG.port,
+  secure: false,
+  auth: {
+    user: SMTP_CONFIG.user,
+    pass: SMTP_CONFIG.pass
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+})
+
+async function run() {
+  const mailSend = transporter.sendMail({
+    text: 'Texto do Email',
+    subject: 'Assunto do Email',
+    from: 'Bruno Passos <brunopassosdeveloper@gmail.com>',
+    to: ['brunopassosdeveloper@gmail.com','bruno.passos@outlook.com.br','bruno.leopold@hotmail.com']
+  },
+  console.log(mailSend)
+  )
+}
---
+++ src/config/smtp.js
@@ -0,0 +1,6 @@
+module.exports = {
+  host: 'smtp.gmail.com',
+  port: 587,
+  user: 'brunopassosdeveloper@gmail.com',
+  pass: '0703@testemmo559*'
+};
