3df0fbed5cbc6fee23dea6c9f857f612a4f11ccd
Darimus <bruno.passos@outlook.com.br>
2023-03-31 11:42:44 -0300

    working on screean behavior

--- src/app/modules/home/components/main-screen-sobre/main-screen-sobre.component.ts
+++ src/app/modules/home/components/main-screen-sobre/main-screen-sobre.component.ts
@@ -9,5 +9,8 @@ export class MainScreenSobreComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const mainSection = document.querySelector("section.main-teste") as HTMLElement;
+    const alturaMain = mainSection.offsetHeight;
+    console.log("Altura da seção main: " + alturaMain + "px");
   }
 }
